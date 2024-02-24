6fcd1ca8a1c435cd156b467f8cf11eb87457ffad
Parth Singh <parth3743@gmail.com>
2024-02-24 18:11:40 +0530

    added tweet model

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,9 +3,9 @@ import bodyParser from "body-parser";
 import { ApolloServer } from "@apollo/server";
 import cors from "cors";
 import { expressMiddleware } from "@apollo/server/express4";
-import { prismaClient } from "../clients/db";
 
 import { User } from "./user";
+import { Tweet } from "./tweet";
 import { GraphqlContext } from "../interfaces";
 import JWTService from "../services/jwt";
 
@@ -18,14 +18,27 @@ export async function initServer() {
     const graphqlServer = new ApolloServer<GraphqlContext>({
         typeDefs: `
             ${User.types}
+            ${Tweet.types}
+
             type Query {
                 ${User.queries}
+                ${Tweet.queries}
+            }
+
+            type Mutation {
+                ${Tweet.mutations}
             }
         `,
         resolvers: {
             Query: {
                 ...User.resolvers.queries,
+                ...Tweet.resolvers.queries
             },
+            Mutation: {
+                ...Tweet.resolvers.mutations
+            },
+            ...Tweet.resolvers.extraResolvers,
+            ...User.resolvers.extraResolvers
         },
     });
 
