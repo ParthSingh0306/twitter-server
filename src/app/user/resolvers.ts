f4578502d1d6742d3e059f7a7fbcf28440901913
Parth Singh <99341523+ParthSingh0306@users.noreply.github.com>
2024-03-21 10:56:14 +0530

    Update resolvers.ts

--- src/app/user/resolvers.ts
+++ src/app/user/resolvers.ts
@@ -1,7 +1,7 @@
 import { prismaClient } from "../../clients/db";
 import { GraphqlContext } from "../../interfaces";
 import { User } from "@prisma/client";
-import UserService from "@src/services/user.ts";
+import UserService from "../../services/user";
 import { redisClient } from "../../clients/redis";
 
 const queries = {
